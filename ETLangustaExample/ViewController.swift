b3c313524ff9e1490df16111f4d2d3ed38b39d21
Petr Urban <petr.urban@gmail.com>
2018-08-17 14:15:01 +0200

    🚧 example labels

--- ETLangustaExample/ViewController.swift
+++ ETLangustaExample/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import ETLangusta
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let localizedLabel = UILabel()
+    let localizedLabel2 = UILabel()
+    let localizedLabel3 = UILabel()
+
+    override func loadView() {
+        super.loadView()
+
+        setupViews()
+
+        let dataProvider = RemoteDataProvider(url: URL(string: "https://api.myjson.com/bins/npnl0")!)
+        let config = Langusta.Config(defaultLanguage: "cz", dataProvider: dataProvider)
+        let langusta = Langusta(config: config)
+
+        localizedLabel.text = "loca1"
+        localizedLabel2.text = "loca2"
+        localizedLabel3.text = "loca3"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func setupViews() {
+        view.addSubview(localizedLabel)
+        localizedLabel.translatesAutoresizingMaskIntoConstraints = false
+        localizedLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        localizedLabel.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        localizedLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true
+
+        view.addSubview(localizedLabel2)
+        localizedLabel2.translatesAutoresizingMaskIntoConstraints = false
+        localizedLabel2.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        localizedLabel2.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        localizedLabel2.topAnchor.constraint(equalTo: localizedLabel.bottomAnchor, constant: 24).isActive = true
+
+        view.addSubview(localizedLabel3)
+        localizedLabel3.translatesAutoresizingMaskIntoConstraints = false
+        localizedLabel3.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        localizedLabel3.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        localizedLabel3.topAnchor.constraint(equalTo: localizedLabel2.bottomAnchor, constant: 24).isActive = true
     }
-
-
 }
-
